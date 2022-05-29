5a1afb8de0e36f4eca9e4e6258d0688f67843aaf
ferca88 <66146523+ferca88@users.noreply.github.com>
2022-05-29 18:43:27 -0500

    valida usuario

--- 06-Code/controller/validarusuario.php
+++ 06-Code/controller/validarusuario.php
@@ -7,13 +7,16 @@ require_once $_SERVER['DOCUMENT_ROOT']."/CANGRECODERS-GROUP-1/06-Code/db/db.php"
 $usuario = $_POST['txtUsuario'] ;
 $password = $_POST['txtPassword'] ;
 
-echo $usuario;
-echo $password;
 $query = new MongoDB\Driver\Query(array('usuario' => $usuario,'password'=>$password));
 
  //Output of the executeQuery will be object of MongoDB\Driver\Cursor class
 $cursor = $manager->executeQuery('gestionEducativa.usuarios', $query);
-
 // Convert cursor to Array and print result
 $users = $cursor->toArray();
-echo count($users);
+$result = count($users);
+
+if ($result != 0) {
+    require_once $_SERVER['DOCUMENT_ROOT']."/CANGRECODERS-GROUP-1/06-Code/views/principal.php";
+}else{
+    require_once $_SERVER['DOCUMENT_ROOT']."/CANGRECODERS-GROUP-1/06-Code/login.php";
+}
