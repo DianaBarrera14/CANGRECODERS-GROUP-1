2c2d42057daf6d004598b2161e1971509e9d5a07
ferca88 <66146523+ferca88@users.noreply.github.com>
2022-05-29 22:41:42 -0500

    materias

---
+++ 06-Code/controller/insertarMateria.php
@@ -0,0 +1,8 @@
+<?php
+
+$materia = $_POST['materia'];
+$year    = $_POST['year'];
+$departamento = $_POST['departamento'];
+$nivel = $_POST['nivel'];
+$paralelo = $_POST['paralelo'];
+$docente = $_POST['docente'];
