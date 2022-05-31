f3b3f6a7bf817d29d227bfe847113a171f56ad71
ferca88 <66146523+ferca88@users.noreply.github.com>
2022-05-31 14:12:00 -0500

    unificación de JS

--- 06-Code/views/js/registroDatos.js
+++ 06-Code/views/js/registroDatos.js
@@ -27,7 +27,45 @@ function registroMaterias (){
 
     })
 
-
-
-
 }
+
+function registroDocente (){
+    var nombre = $('#txtNombre').val();
+    var apellidos = $('#txtApellido').val();
+    var nivel = $('#txtNivel').val();
+    var telefono    = $('#txtTelefono').val();
+    var direccion = $('#txtDireccion').val();
+    var email = $('#txtCorreo').val();
+    var paralelo = $('#txtParalelos').val();
+    var docente = $('#txtDocente').val();
+    var materias = $('#txtMaterias').val();
+    var year    = $('#yearLectivo').val();
+    var nummaterias = $('#numMateria').val(); 
+    var salario = $('#numSalario').val(); 
+    var usuario = $('#txtUsuario').val();
+    var password = $('#txtPassword').val();
+    var tipo_usuario = $('#txtTipo').val();
+    var estatus = $('#txtStatus').val();
+
+
+    var url = "http://localhost/CANGRECODERS-GROUP-1/06-Code/controller/insertarDocente.php"
+
+    $.ajax({
+        type: 'POST',
+        url : url,
+        data : {'nombre':nombre,'apellidos':apellidos,'nivel':nivel,'telefono':telefono,'direccion':direccion,'paralelo':paralelo,'docente':docente,'materias':materias,'year':year,'nummaterias':nummaterias,'salario':salario,'usuario':usuario,'password':password,'tipo_usuario':tipo_usuario,'estatus':estatus},
+        success : function (response)
+        {
+         if (response == 'registrado') {
+            $('#myModal').modal('show');
+         }else{
+            alert(error);
+
+         }
+
+
+        }
+
+
+
+    })
