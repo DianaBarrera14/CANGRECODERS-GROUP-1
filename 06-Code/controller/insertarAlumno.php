612e9d4b5ef560d3912baae44f3c4baf406d0130
karlacazares <kgcazares@espe.edu.ec>
2022-06-15 21:37:13 -0500

    datos

--- 06-Code/controller/insertarAlumno.php
+++ 06-Code/controller/insertarAlumno.php
@@ -1,34 +1,34 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT']."/CANGRECODERS-GROUP-1/06-Code/db/db.php";
 
-$nombre = $_POST['nombre'];
-$apellidos = $_POST['apellidos'];
+$name = $_POST['nombre'];
+$lastName = $_POST['apellidos'];
 //$nivel = $_POST['curso'];
-$telefono    = $_POST['telefono'];
-$direccion = $_POST['direccion'];
-$paralelo = $_POST['paralelo'];
-$cedula = $_POST['cedula'];
+$phone    = $_POST['telefono'];
+$address = $_POST['direccion'];
+$parallel = $_POST['paralelo'];
+$identificationCard = $_POST['cedula'];
 //$materias = $_POST['materias'];
 $year = $_POST['year'];
 $nummaterias = $_POST['nummaterias'];
 //$salario = $_POST['salario'];
-$usuario = $_POST['usuario'];
+$user = $_POST['usuario'];
 $password = $_POST['password'];
 $tipo_usuario = $_POST['tipo_usuario'];
 $estatus = $_POST['estatus'];
 
 
 $obj = array (
-'nombre' => $nombre,
-'apellidos' => $apellidos,
+'nombre' => $name,
+'apellidos' => $lastName,
 //'curso' => $nivel,
-'telefono' => $telefono,
-'direccion'=> $direccion,
-'paralelo'=> $paralelo,
-'cedula' => $cedula,
+'telefono' => $phone,
+'direccion'=> $address,
+'paralelo'=> $parallel,
+'cedula' => $identificationCard,
 'yearLectivo'=>$year,
 'numMaterias'=>$nummaterias,
-'usuario'=>$usuario,
+'usuario'=>$user,
 'password'=>$password,
 'tipo_usuario'=>$tipo_usuario,
 'estatus'=>$estatus
