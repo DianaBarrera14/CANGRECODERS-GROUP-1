c2ae2eb0f8a2d3e265e85d87499206fb76bc549e
ferca88 <66146523+ferca88@users.noreply.github.com>
2022-07-24 16:29:01 -0500

    web services to node

--- 06-Code/03WebAppReact/educationapp/src/App.js
+++ 06-Code/03WebAppReact/educationapp/src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="wrapper fadeInDown">
+  <div id="formContent">
+      <div className="fadeIn first">
+      <img className="image-login" src="https://findicons.com/files/icons/1035/human_o2/128/gdm_login_photo.png" id="icon" alt="User Icon" />
     </div>
+
+    
+    <form  onsubmit="event.preventDefault()">
+      <input type="text" id="txtUsuario" class="fadeIn second" name="txtUsuario" placeholder="Usuario"></input>
+      <input type="password" id="txtPassword" class="fadeIn third" name="txtPassword" placeholder="Contraseña"></input>
+      <button onclick="validaUsuario()" type="submit" class="btn-primary">Ingresar</button>
+    </form>
+
+ 
+
+  </div>
+</div>
   );
 }
 
