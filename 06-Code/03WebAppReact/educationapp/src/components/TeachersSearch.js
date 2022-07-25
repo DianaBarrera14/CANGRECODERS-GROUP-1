0a998e4dd26dabc1f4b570c56b76de80638ee170
Karla Cazares <kgcazares@espe.edu.ec>
2022-07-25 16:42:36 -0500

    profesores

---
+++ 06-Code/03WebAppReact/educationapp/src/components/TeachersSearch.js
@@ -0,0 +1,71 @@
+import { getAllByText } from '@testing-library/react';
+import React, { useEffect, useState } from 'react';
+
+
+const TeachersSearch = () => {
+    const [client, setClient] = useState();
+    const url = "http://localhost:3003/educationsystem/profesores";
+    const fetchApi = async () => {
+        const response = await fetch(url)
+        const responseJSON = await response.json()
+        setClient(responseJSON)
+        //console.log(responseJSON)
+    }
+    useEffect(() => {
+        fetchApi()
+    }, []);
+
+
+
+    return (
+        <div>
+            <div class="card-body">
+                    <br />
+                    <br />
+                    <h3 className="text-center">GET TEACHERS INFORMATION FROM API</h3>
+                    <br />
+                    <h5>This shows teachers information from API: {url}</h5>
+                    <br />
+                    <br />
+                </div>
+                
+            <table className="table table-hover">
+                <thead>
+                    <tr className="text-center">
+                        <th scope="col" >id</th>
+                        <th scope="col">name</th>
+                        <th scope="col">lastname</th>
+                        <th scope="col">user</th>
+                        <th scope="col">password</th>
+                        <th scope="col">type user</th>
+                        <th scope="col">status</th>
+
+
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {!client ? 'Loading' :
+                        client.map((client, index) => {
+                            return <tr className="table-info" >
+                                <td className="text-center">{client.idCedula}</td>
+                                <td className="text-center">{client.name}</td>
+                                <td className="text-center">{client.lastName}</td>
+                                <td className="text-center">{client.user}</td>
+                                <td className="text-center">{client.password}</td>
+                                <td className="text-center">{client.type_user}</td>
+                                <td className="text-center">{client.status}</td>
+
+                            </tr>
+                        }
+
+                        )
+                    }
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
+
+export default TeachersSearch;
--- 06-Code/03WebAppReact/educationapp/src/pages/profesores.js
+++ 06-Code/03WebAppReact/educationapp/src/pages/profesores.js
@@ -1,9 +1,132 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+import TeachersSearch from '../components/TeachersSearch';
+
+const baseUrl = "http://localhost:3000/educationsystem/user";
+const cookies = new Cookies();
+//const [client, setClient] = useState(null);
+const url="http://localhost:3000/educationsystem/profesores";
 
 export default class profesores extends Component {
-  render() {
+
+  
+    /* Future post
+    state = {
+      form: {
+        txtUsuario: "",
+        txtPassword: "",
+      },
+    };
+
+    handleChange = async (e) => {
+      await this.setState({
+        form: {
+          ...this.state.form,
+          [e.target.name]: e.target.value,
+        },
+      });
+    };
+    putTeachers = async () => {
+      await axios
+        .post(baseUrl, {
+          params: {
+            idCedula: this.state.form.txtCedula,
+            user: this.state.form.txtUsuario,
+            password: this.state.form.txtPassword,
+            name: this.state.form.txtNombre,
+            lastname: this.state.form.txtApellido,
+            type_user: this.state.form.txtTipo,
+            status: this.state.form.txtStatus
+          },
+        })
+        .then((response) => {
+          return response.data;
+        })
+        .then((response) => {
+          if (response) {
+            cookies.set("id", response.userid, { path: "/profesores" });
+            cookies.set("cedula", response.cedula, { path: "/profesores" });
+            cookies.set("name", response.name, { path: "/uprofesores" });
+            cookies.set("lastName", response.lastName, { path: "/profesores" });
+            alert(`Bienvenido ${response.name} ${response.lastName}`);
+            window.location.href = "./profesores";
+          } else {
+            alert("El usuario o la contraseña son incorrectos");
+          }
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    };
+
+    handleChange = async (e) => {
+      await this.setState({
+        form: {
+          ...this.state.form,
+          [e.target.name]: e.target.value,
+        },
+      });
+    };
+
+    componentDidMount(){
+      if (cookies.put('id')) {
+        window.location.href = "./profesor";
+      }
+    }*/
+
+
+    
+    render() {
     return (
-      <div>profesores</div>
+
+
+      <div>
+        <TeachersSearch />
+        <div class="container-fluid px-1 py-5 mx-auto">
+          <div class="row d-flex justify-content-center">
+            <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
+              <h3>REGISTRO DE PROFESORES</h3>
+              <p class="blue-text">Complete todos los campos para registrar al Profesor</p>
+              <div class="card">
+
+                <form class="form-card" >
+                  <div class="row justify-content-between text-left">
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Cédula Profesor<span class="text-danger"> *</span></label> <input type="text" id="txtCedula" name="txtCedula" placeholder="ej: 0704556920" onChange={this.handleChange} />
+                    </div>
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Nombre de usuario<span class="text-danger"> *</span></label> <input type="text" id="txtUsuario" name="txtUsuario" placeholder="Inicial del nombre y apellido, Ej: jcarrera" onChange={this.handleChange} />
+                    </div>
+                  </div>
+                  <div class="row justify-content-between text-left">
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Contraseña<span class="text-danger"> *</span></label> <input type="text" id="txtPassword" name="txtPassword" placeholder="Número de cédula, EJ: 1600894628" onChange={this.handleChange} />
+                    </div>
+                  </div>
+
+                  <div class="row justify-content-between text-left">
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Nombres del profesor<span class="text-danger"> *</span></label> <input type="text" id="txtNombre" name="txtNombre" placeholder="Nombres del Profesor" onChange={this.handleChange} /> </div>
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Apellidos del profesor<span class="text-danger"> *</span></label> <input type="text" id="txtApellido" name="txtApellido" placeholder="Apellidos del Profesor " onChange={this.handleChange} /> </div>
+                  </div>
+                  <div class="row justify-content-between text-left">
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Tipo de usuario<span class="text-danger"> *</span></label> <input type="text" id="txtTipo" name="txtTipo" placeholder="Ej: docente, alumno" onChange={this.handleChange} /> </div>
+                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">
+                      Status<span class="text-danger"> *</span></label> <input type="text" id="txtStatus" name="txtStatus" placeholder="Ej: activo, inactivo" onChange={this.handleChange} /> </div>
+                  </div>
+                  <div class="row justify-content-center">
+                    <div class="form-group col-sm-6"> <button onClick={() => this.putTeacher()} type="submit" class="btn-block btn-primary">Registrar Profesor</button> </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
