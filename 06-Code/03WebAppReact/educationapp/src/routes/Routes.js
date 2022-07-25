8c4bb7431750ab6d66dfb0a0dc316b2a31fc8273
ferca88 <66146523+ferca88@users.noreply.github.com>
2022-07-25 18:19:29 -0500

    routes principañ

--- 06-Code/03WebAppReact/educationapp/src/routes/Routes.js
+++ 06-Code/03WebAppReact/educationapp/src/routes/Routes.js
@@ -11,17 +11,10 @@ function App() {
     <Router>
       <Routes>
         <Route exact path='/' element={<Login/>} />
-<<<<<<< HEAD
-        <><Route exact path='/menu' element={<Menu />} /><Route exact path='/profesores' element={<Profesores />} /><Route exact path='/alumnos' element={<Alumnos />} /><Route exact path='/usuarios' element={<Usuarios />} /></> 
-=======
-        <Route exact path='/menu' element={<Menu/>} /> 
-        <Route exact path='/profesores' element={<Profesores/>} /> 
-        <Route exact path='/alumnos' element={<Alumnos/>} /> 
-<<<<<<< HEAD
-        <Route exact path='/usuarios' element={<Usuarios/>} /> 
-=======
->>>>>>> 0a998e4dd26dabc1f4b570c56b76de80638ee170
->>>>>>> 11d4eb6f50afe554b294693dbc8dc9202573caca
+        <Route exact path='/menu' element={<Menu/>}/> 
+        <Route exact path='/profesores' element={<Profesores/>}/> 
+        <Route exact path='/alumnos' element={<Alumnos/>}/> 
+        <Route exact path='/usuarios' element={<Usuarios/>}/> 
       </Routes>
     </Router>
   );
