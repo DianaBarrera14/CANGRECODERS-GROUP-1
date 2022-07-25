b2f1317d72a1f0a4f2a5d917ef4e00d55d9350b2
ferca88 <66146523+ferca88@users.noreply.github.com>
2022-07-25 12:58:32 -0500

    correction loop

--- 06-Code/03WebAppReact/educationapp/src/pages/menu.js
+++ 06-Code/03WebAppReact/educationapp/src/pages/menu.js
@@ -12,8 +12,8 @@ class menu extends Component {
         window.location.href='./'
     }
 
-    componentDidMount(){
-        if(!cookies.get('id')){
+     componentDidCatch(){
+        if(!cookies.get('cedula')){
             window.location.href="./menu";
         }
     }
@@ -29,29 +29,18 @@ class menu extends Component {
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
-           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+      <li className="nav-item">
+        <button className='btn btn-primary' onClick={()=>this.cerrarSesion()}>Cerrar Sesión</button>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
+      
     </ul>
   
   </div>
